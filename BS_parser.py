ec1eab7a9a07b4aed0b986f3d13e5f726ad9a767
Clarivy <zqc.sc.hn@gmail.com>
2023-01-27 11:09:19 +0800

    Add: support export LiveLinkFace csv

--- BS_parser.py
+++ BS_parser.py
@@ -4,6 +4,8 @@ from facedriver.timecode import Timecode
 NAMES_ARKIT = ['EyeBlinkLeft', 'EyeLookDownLeft', 'EyeLookInLeft', 'EyeLookOutLeft', 'EyeLookUpLeft', 'EyeSquintLeft', 'EyeWideLeft', 'EyeBlinkRight', 'EyeLookDownRight', 'EyeLookInRight', 'EyeLookOutRight', 'EyeLookUpRight', 'EyeSquintRight', 'EyeWideRight', 'JawForward', 'JawRight', 'JawLeft', 'JawOpen', 'MouthClose', 'MouthFunnel', 'MouthPucker', 'MouthRight', 'MouthLeft', 'MouthSmileLeft', 'MouthSmileRight', 'MouthFrownLeft', 'MouthFrownRight', 'MouthDimpleLeft', 'MouthDimpleRight', 'MouthStretchLeft', 'MouthStretchRight', 'MouthRollLower', 'MouthRollUpper', 'MouthShrugLower', 'MouthShrugUpper', 'MouthPressLeft', 'MouthPressRight', 'MouthLowerDownLeft', 'MouthLowerDownRight', 'MouthUpperUpLeft', 'MouthUpperUpRight', 'BrowDownLeft', 'BrowDownRight', 'BrowInnerUp', 'BrowOuterUpLeft', 'BrowOuterUpRight', 'CheekPuff', 'CheekSquintLeft', 'CheekSquintRight', 'NoseSneerLeft', 'NoseSneerRight', 'TongueOut', 'HeadYaw', 'HeadPitch', 'HeadRoll', 'LeftEyeYaw', 'LeftEyePitch', 'LeftEyeRoll', 'RightEyeYaw', 'RightEyePitch', 'RightEyeRoll']
 NAMES_USC55 = ["browDown_L", "browDown_R", "browInnerUp_L", "browInnerUp_R", "browOuterUp_L", "browOuterUp_R", "cheekPuff_L", "cheekPuff_R", "cheekRaiser_L", "cheekRaiser_R", "cheekSquint_L", "cheekSquint_R", "eyeBlink_L", "eyeBlink_R", "eyeLookDown_L", "eyeLookDown_R", "eyeLookIn_L", "eyeLookIn_R", "eyeLookOut_L", "eyeLookOut_R", "eyeLookUp_L", "eyeLookUp_R", "eyeSquint_L", "eyeSquint_R", "eyeWide_L", "eyeWide_R", "jawForward", "jawLeft", "jawOpen", "jawRight", "mouthClose", "mouthDimple_L", "mouthDimple_R", "mouthFrown_L", "mouthFrown_R", "mouthFunnel", "mouthLeft", "mouthLowerDown_L", "mouthLowerDown_R", "mouthPress_L", "mouthPress_R", "mouthPucker", "mouthRight", "mouthRollLower", "mouthRollUpper", "mouthShrugLower", "mouthShrugUpper", "mouthSmile_L", "mouthSmile_R", "mouthStretch_L", "mouthStretch_R", "mouthUpperUp_L", "mouthUpperUp_R", "noseSneer_L", "noseSneer_R"]
 NAMES_USC55_from_ARKIT = { "browDown_L": "BrowDownLeft", "browDown_R": "BrowDownRight", "browInnerUp_L": "BrowInnerUp", "browInnerUp_R": "BrowInnerUp", "browOuterUp_L": "BrowOuterUpLeft", "browOuterUp_R": "BrowOuterUpRight", "cheekPuff_L": "CheekPuff", "cheekPuff_R": "CheekPuff", "cheekRaiser_L": "", "cheekRaiser_R": "", "cheekSquint_L": "CheekSquintLeft", "cheekSquint_R": "CheekSquintRight", "eyeBlink_L": "EyeBlinkLeft", "eyeBlink_R": "EyeBlinkRight", "eyeLookDown_L": "EyeLookDownLeft", "eyeLookDown_R": "EyeLookDownRight", "eyeLookIn_L": "EyeLookInLeft", "eyeLookIn_R": "EyeLookInRight", "eyeLookOut_L": "EyeLookOutLeft", "eyeLookOut_R": "EyeLookOutRight", "eyeLookUp_L": "EyeLookUpLeft", "eyeLookUp_R": "EyeLookUpRight", "eyeSquint_L": "EyeSquintLeft", "eyeSquint_R": "EyeSquintRight", "eyeWide_L": "EyeWideLeft", "eyeWide_R": "EyeWideRight", "jawForward": "JawForward", "jawLeft": "JawLeft", "jawOpen": "JawOpen", "jawRight": "JawRight", "mouthClose": "MouthClose", "mouthDimple_L": "MouthDimpleLeft", "mouthDimple_R": "MouthDimpleRight", "mouthFrown_L": "MouthFrownLeft", "mouthFrown_R": "MouthFrownRight", "mouthFunnel": "MouthFunnel", "mouthLeft": "MouthLeft", "mouthLowerDown_L": "MouthLowerDownLeft", "mouthLowerDown_R": "MouthLowerDownRight", "mouthPress_L": "MouthPressLeft", "mouthPress_R": "MouthPressRight", "mouthPucker": "MouthPucker", "mouthRight": "MouthRight", "mouthRollLower": "MouthRollLower", "mouthRollUpper": "MouthRollUpper", "mouthShrugLower": "MouthShrugLower", "mouthShrugUpper": "MouthShrugUpper", "mouthSmile_L": "MouthSmileLeft", "mouthSmile_R": "MouthSmileRight", "mouthStretch_L": "MouthStretchLeft", "mouthStretch_R": "MouthStretchRight", "mouthUpperUp_L": "MouthUpperUpLeft", "mouthUpperUp_R": "MouthUpperUpRight", "noseSneer_L": "NoseSneerLeft", "noseSneer_R": "NoseSneerRight", }
+ARKIT_CSV_HEADER = ['Timecode', 'BlendShapeCount', 'EyeBlinkLeft', 'EyeLookDownLeft', 'EyeLookInLeft', 'EyeLookOutLeft', 'EyeLookUpLeft', 'EyeSquintLeft', 'EyeWideLeft', 'EyeBlinkRight', 'EyeLookDownRight', 'EyeLookInRight', 'EyeLookOutRight', 'EyeLookUpRight', 'EyeSquintRight', 'EyeWideRight', 'JawForward', 'JawRight', 'JawLeft', 'JawOpen', 'MouthClose', 'MouthFunnel', 'MouthPucker', 'MouthRight', 'MouthLeft', 'MouthSmileLeft', 'MouthSmileRight', 'MouthFrownLeft', 'MouthFrownRight', 'MouthDimpleLeft', 'MouthDimpleRight', 'MouthStretchLeft', 'MouthStretchRight', 'MouthRollLower', 'MouthRollUpper', 'MouthShrugLower', 'MouthShrugUpper', 'MouthPressLeft', 'MouthPressRight', 'MouthLowerDownLeft', 'MouthLowerDownRight', 'MouthUpperUpLeft', 'MouthUpperUpRight', 'BrowDownLeft', 'BrowDownRight', 'BrowInnerUp', 'BrowOuterUpLeft', 'BrowOuterUpRight', 'CheekPuff', 'CheekSquintLeft', 'CheekSquintRight', 'NoseSneerLeft', 'NoseSneerRight', 'TongueOut', 'HeadYaw', 'HeadPitch', 'HeadRoll', 'LeftEyeYaw', 'LeftEyePitch', 'LeftEyeRoll', 'RightEyeYaw', 'RightEyePitch', 'RightEyeRoll'] 
+ARKIT_BLENDSHAPES_COUNT = len(NAMES_ARKIT)
 
 class DataFrameWrapper():
     
@@ -13,7 +15,8 @@ class DataFrameWrapper():
         self._byteorder = byteorder
         self.float_format = (">" if self._byteorder == 'big' else "<") + "f4"
 
-    def parse(self, data: np.ndarray) -> None:
+    def parse_bytearray(self, data: np.ndarray) -> bytearray:
+        data = self.parse_ndarray(data)
         package = bytearray()
         BS_NUM = len(data)
         # data[-9:] *= 0
@@ -21,6 +24,9 @@ class DataFrameWrapper():
         package.extend(BS_NUM_BYTES)
         package.extend(data.astype(self.float_format).tobytes('C'))
         return package
+    
+    def parse_ndarray(self, data: np.ndarray) -> np.ndarray:
+        return data
         
     def set_byteorder(self, byteorder: str) -> None:
         self._byteorder = byteorder
@@ -34,12 +40,12 @@ class ExtendDataFrameWrapper(DataFrameWrapper):
         super().__init__(byteorder=byteorder)
         self._data_len = data_len
     
-    def parse(self, data: np.ndarray) -> bytearray:
+    def parse_ndarray(self, data: np.ndarray) -> np.ndarray:
         if len(data) < self._data_len:
             data = np.pad(data, (0, self._data_len - len(data)), 'constant', constant_values=(0., 0.))
         if len(data) > self._data_len:
             raise Exception("Data is too long for this package")
-        return super().parse(data)
+        return data
 
 class USC55_to_ARKIT_Wrapper(DataFrameWrapper):
 
@@ -59,7 +65,7 @@ class USC55_to_ARKIT_Wrapper(DataFrameWrapper):
                 self._remap_list[ARKIT_index] = index
         
     
-    def parse(self, data: np.ndarray) -> None:
+    def parse_ndarray(self, data: np.ndarray) -> np.ndarray:
         # transform from USC55 to ARKIT 61
         # The missing part can be ignored
         if len(data) != 55:
@@ -69,7 +75,7 @@ class USC55_to_ARKIT_Wrapper(DataFrameWrapper):
         extended_data = np.pad(data, (0, 6), 'constant', constant_values=(0., 0.5))
         ARKIT_data = extended_data[self._remap_list]
         ARKIT_data[-9:] = np.random.randn(9) / 100000
-        return super().parse(ARKIT_data)
+        return ARKIT_data
 
         # extended_data = np.pad(np.ones(55) / 10, (0, 6), 'constant', constant_values=(0., 0.5))
         # return super().parse(np.ones(61))
@@ -136,7 +142,27 @@ class BS_Parser():
 
         return package_header
 
-
+    def export_ARKIT_csv(self, csv_filename: str) -> None:
+        """
+        Dump data buffer into ARKIT csv file
+        The data must be in ARKIT format
+        """
+        with open(csv_filename, 'w') as f:
+            # Write csv header
+            f.write(','.join(ARKIT_CSV_HEADER) + '\n')
+            # Write csv body
+            while self.current_index < len(self.data_buffer):
+                # Timecode
+                f.write(str(self.timecode) + ',')
+                # BlendShape Count
+                parsed_data = self.get_ndarray_data()
+                data_len = len(parsed_data)
+                if data_len != ARKIT_BLENDSHAPES_COUNT:
+                    raise Exception(f"Data length check failed: {data_len} != {ARKIT_BLENDSHAPES_COUNT}")
+                f.write(str(data_len) + ',')
+                f.write(','.join([str(x) for x in parsed_data]) + '\n')
+                self.next_tick()
+        
     
     def parse_npy(self, npy_filename: str) -> None:
         """
@@ -154,7 +180,28 @@ class BS_Parser():
             self.data_buffer = npy_data
         else:
             self.data_buffer = np.concatenate((self.data_buffer, npy_data), axis=0)
-        
+    
+    def next_tick(self) -> None:
+        """
+        Update timecode and current index
+        """
+
+        self.timecode.__next__()
+        self.current_index += 1
+
+    def get_ndarray_data(self) -> np.ndarray:
+        """
+        Return current dataframe
+        """
+        target_data = self.data_buffer[self.current_index]
+        return self.dataframe_wrapper.parse_ndarray(target_data)
+    
+    def get_bytearray_data(self) -> bytearray:
+        """
+        Return current dataframe
+        """
+        target_data = self.data_buffer[self.current_index]
+        return self.dataframe_wrapper.parse_bytearray(target_data)
     
     def __iter__(self) -> 'BS_Parser':
         return self
@@ -176,12 +223,10 @@ class BS_Parser():
         package.extend(self.timecode.get_timecode_bytes())
 
         # Data
-        target_data:np.ndarray = self.data_buffer[self.current_index]
-        package.extend(self.dataframe_wrapper.parse(target_data))
+        package.extend(self.get_bytearray_data())
 
         # Update time
-        self.current_index += 1
-        # self.timecode.__next__()
+        self.next_tick()
 
         return package
     
